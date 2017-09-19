8b956fd40171db39075dfb0b04e14f26f3b6b92a
Hilmar Finnsson <hilmarfinnsson@gmail.com>
2017-09-19 10:01:02 +0000

    Just to make it run correctly with telegram

--- distribrewed/master/master.py
+++ distribrewed/master/master.py
@@ -4,7 +4,7 @@ from distribrewed_core.base.master import BaseMaster
 class DistribrewedMaster(BaseMaster):
     def send_telgram_message(self, message):
         self._call_worker_method(
-            worker_id='pi_telegram',
+            worker_id='worker.pi_telegram',
             method='telegram_bot_send_message',
             args=[message]
         )
