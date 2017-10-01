431551183e9982ff4911a1dcccd7d4c10bf8d209
Guðmundur Björn Birkisson <gudmundurbb@gagnaveita.is>
2017-10-01 22:50:28 +0000

    Auto create admin user if no user is available

--- distribrewed/masters/apps.py
+++ distribrewed/masters/apps.py
@@ -7,3 +7,14 @@ class MasterConfig(AppConfig):
     def ready(self):
         # noinspection PyUnresolvedReferences
         import masters.signals
+        self.create_super_user()
+
+    @staticmethod
+    def create_super_user():
+        # noinspection PyBroadException
+        try:
+            from django.contrib.auth.models import User
+            if len(User.objects.all()) == 0:
+                User.objects.create_superuser('admin', '', 'admin')
+        except Exception:
+            pass
